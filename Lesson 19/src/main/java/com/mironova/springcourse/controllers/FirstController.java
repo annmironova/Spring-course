b05e6e8e5c8bf3ae4cfa26ec48de5a6c301159c1
annmironova <58204825+annmironova@users.noreply.github.com>
2022-06-07 14:40:48 +0300

    Add files via upload

---
+++ Lesson 19/src/main/java/com/mironova/springcourse/controllers/FirstController.java
@@ -0,0 +1,25 @@
+package com.mironova.springcourse.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping("/first")
+public class FirstController {
+
+    @GetMapping("/hello")
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname) {
+        System.out.println("hello, " + name + " " + surname);
+        return "first/hello";
+    }
+
+    @GetMapping("/goodbye")
+    public String goodbyePage() {
+        return "first/goodbye";
+    }
+}
