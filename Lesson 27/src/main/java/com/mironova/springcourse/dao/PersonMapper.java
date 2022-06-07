c50ac10855757181260f9bfd22214057e40b0a88
annmironova <58204825+annmironova@users.noreply.github.com>
2022-06-07 14:46:01 +0300

    Add files via upload

---
+++ Lesson 27/src/main/java/com/mironova/springcourse/dao/PersonMapper.java
@@ -0,0 +1,19 @@
+package com.mironova.springcourse.dao;
+
+import com.mironova.springcourse.models.Person;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class PersonMapper implements RowMapper<Person> {
+    @Override
+    public Person mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Person person = new Person();
+        person.setId(rs.getInt("id"));
+        person.setName(rs.getString("name"));
+        person.setEmail(rs.getString("email"));
+        person.setAge(rs.getInt("age"));
+        return person;
+    }
+}
